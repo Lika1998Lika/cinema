07fda53a9185f2eae2f2bbfde1a9b05ed894999c
Anzhelika <anzhelika_vanuyto@mail.ru>
2024-08-22 18:32:52 +0500

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log('1')
+import {render} from './render.js';
+import FilmsPresenter from './presenter/films-presenter.js';
+import HeaderProfileView from "./view/header-profile-view.js";
+import FilterView from "./view/filter-view.js";
+
+const bodyElement = document.querySelector('body');
+const siteMainElement = bodyElement.querySelector('.main');
+const siteHeaderElement = bodyElement.querySelector('.header');
+
+const filmsPresenter = new FilmsPresenter();
+
+render(new FilterView(), siteMainElement);
+render(new HeaderProfileView(), siteHeaderElement);
+
+filmsPresenter.init(siteMainElement);
---
+++ src/mock/tasks.js
@@ -0,0 +1,7 @@
+import { getRandomInteger } from "../utils.js"
+export const localComment = () => (
+ {
+    comment: "a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.",
+    emotion: "smile"
+  }
+)
